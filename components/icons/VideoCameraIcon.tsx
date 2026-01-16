b05af6eb8e1149c6c4c06c8301648da7415485d5
Emeke John <jomekgroup@gmail.com>
2026-01-16 13:04:27 +0100

    Fix: SendIcon import path and case sensitivity

--- components/icons/VideoCameraIcon.tsx
+++ components/icons/VideoCameraIcon.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export const VideoCameraIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9A2.25 2.25 0 004.5 18.75z" />
+const VideoCameraIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+        <path d="M17 10.5V6a2 2 0 00-2-2H3a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-4.5l4 4v-11l-4 4z" />
     </svg>
 );
+
+export default VideoCameraIcon;
